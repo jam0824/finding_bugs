60c6376f67bdd84e22597a18594f2ca7e2d7808b
htsign <htsign@outlook.com>
2015-12-08 12:58:40 +0900

    htmlプロパティのリファクタ

--- scr/utility.js
+++ scr/utility.js
@@ -2,15 +2,13 @@ if (!HTMLElement.prototype.hasOwnProperty("html")) {
     Object.defineProperty(HTMLElement.prototype, "html", {
         enumerable: true,
         set: function(source) {
-            while (this.hasChildNodes()) {
-                this.removeChild(this.firstChild);
-            }
-            
-            if (typeof source !== "string" || source instanceof String || !source) return;
-            
             var doc = this.ownerDocument || document;
             var range = doc.createRange();
-            range.selectNode(doc.body);
+            
+            range.selectNodeContents(this);
+            range.deleteContents();
+            
+            if (typeof source !== "string" || source instanceof String || !source) return;
             
             var df = range.createContextualFragment(source);
             this.appendChild(df);
